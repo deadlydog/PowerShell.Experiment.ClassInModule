082e1d6cd554461d1ad3d2127a7732defacc817a
deadlydog <deadlydog@hotmail.com>
2023-08-15 02:33:01 -0600

    chore: Rename class file to psm1 to be properly imported by "using module" statement

--- src/ModuleWithClassInSeparateFileIncludedWithUsing/ModuleWithClassInSeparateFileIncludedWithUsing.psm1
+++ src/ModuleWithClassInSeparateFileIncludedWithUsing/ModuleWithClassInSeparateFileIncludedWithUsing.psm1
@@ -1,4 +1,4 @@
-using module .\Classes\MyClassInSeparateFileIncludedWithUsing.psm1
+using module .\Classes\MyClassInSeparateFileIncludedWithUsing.ps1
 
 function Get-MyClassInSeparateFileIncludedWithUsing
 {
